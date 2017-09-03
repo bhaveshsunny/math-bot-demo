bece2eb51085b5cc8a6cf9d763317d4dd64a4e53
bhavesh narra <bhaveshsunny60@yahoo.com>
2017-09-04 01:51:24 +0530

    added speech bubbles and all the required components

--- src/js/add.jsx
+++ src/js/add.jsx
@@ -1,12 +1,4 @@
-import _ from 'lodash';
-import $ from 'jquery';
-import 'bootstrap/dist/js/bootstrap';
-import 'bootswatch/flatly/bootstrap.css';
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './css/style.css';
-
-const mountmbc = $('#MBroot').get(0);
 
 export class MBroot extends React.Component {
 
@@ -34,10 +26,7 @@ export class MBroot extends React.Component {
         <div className="col-lg-3 bots">
         </div>
         <div className="col-lg-9 human">
-          <div className="row output">
-            <div className="message bot-bubble">Hello Human!</div>
-            <div className="message human-bubble">Hello bot!</div>
-          </div>
+          <div className="row output"></div>
           <div className="row input">
 
           </div>
@@ -46,5 +35,3 @@ export class MBroot extends React.Component {
     );
   }
 }
-
-ReactDOM.render(<MBroot/>, mountmbc);

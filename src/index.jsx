f4bd978614aae1aff64bf4eda64148deeafe37da
bhavesh narra <bhaveshsunny60@yahoo.com>
2017-09-05 03:17:04 +0530

    arithmetic working

--- src/index.jsx
+++ src/index.jsx
@@ -5,6 +5,7 @@ import 'bootswatch/flatly/bootstrap.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './css/style.css';
+import { Input } from './js/input.jsx';
 
 const mountmbc = $('#MBroot').get(0);
 
@@ -13,38 +14,23 @@ export class MBroot extends React.Component {
   constructor(props) {
     super(props);
     this.state = {bot: 0};
-    this.handleSubmit = this.handleSubmit.bind(this);
   }
 
   handleClick(i,event) {
-    var self = this;
-    self.setState(prevState => ({
-      bot: i
-    }));
+    this.setState({ bot: i });
   }
 
-  handleSubmit(event) {
-    event.preventDefault();
-    this.props.onCall(this.state.value.split("\n"));
-
-  }
 
   render(){
     return(
       <div className="container">
         <div className="col-lg-3 bots">
-          <div className="add"> <button onClick={this.handleClick.bind(this,0)} className={this.state.bot == 0 ? 'btn btn-success' : 'btn'}>ADD</button></div>
-          <div className="multipy"><button onClick={this.handleClick.bind(this,1)} className={this.state.bot == 1 ? 'btn btn-success' : 'btn'}>MULTIPLY</button></div>
-          <div className="subtract"><button onClick={this.handleClick.bind(this,2)} className={this.state.bot == 2 ? 'btn btn-success' : 'btn'}>SUBTRACT</button></div>
+          <div className="add bot"> <button onClick={this.handleClick.bind(this,0)} className={this.state.bot == 0 ? 'btn btn-success' : 'btn'}>ADD</button></div>
+          <div className="multipy bot"><button onClick={this.handleClick.bind(this,1)} className={this.state.bot == 1 ? 'btn btn-success' : 'btn'}>MULTIPLY</button></div>
+          <div className="subtract bot"><button onClick={this.handleClick.bind(this,2)} className={this.state.bot == 2 ? 'btn btn-success' : 'btn'}>SUBTRACT</button></div>
         </div>
-        <div className="col-lg-9 human">
-          <div className="row output">
-            <div className="message bot-bubble">Hello Human!</div>
-            <div className="message human-bubble">Hello bot!</div>
-          </div>
-          <div className="row input">
-
-          </div>
+        <div className="col-lg-9 col-md-9 col-sm-9 col-xs-9 human">
+          <Input bot={this.state.bot}/>
         </div>
       </div>
     );
--- src/js/input.jsx
+++ src/js/input.jsx
@@ -1,1 +1,73 @@
 import React from 'react';
+
+export class Input extends React.Component{
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: '',
+      numbers: [],
+      output: [],
+      mode: this.props.bot
+  };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.display = this.display.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({value:event.target.value});
+  }
+
+  componentWillReceiveProps(nextprops){
+    this.setState({mode:nextprops.bot});
+    console.log(this.state.mode);
+  }
+
+
+
+  handleSubmit(event) {
+    event.preventDefault();
+    this.state.numbers.push(parseInt(this.state.value));
+    this.state.output.push([parseInt(this.state.value),1]);
+    if (this.state.numbers.length == 2){
+      if(this.state.mode == 0){
+        this.display(this.state.numbers[0]+this.state.numbers[1]);
+      }
+      if(this.state.mode == 1){
+        this.display(this.state.numbers[0]*this.state.numbers[1]);
+      }
+      if(this.state.mode == 2){
+        this.display(this.state.numbers[0]-this.state.numbers[1]);
+      }
+    }
+
+  }
+
+  display(res){
+    this.state.result = res;
+    this.state.numbers = [];
+    this.state.output.push([parseInt(this.state.result),0]);
+    console.log(this.state.result,this.state.output);
+  }
+
+  render(){
+    return(
+      <div>
+        <div className="row output" id="out">
+
+        </div>
+        <div className="row input">
+          <form onSubmit={this.handleSubmit.bind(this)}>
+            <div className="inputform">
+              <input type="number" className="form-control" onChange={this.handleChange.bind(this)} value={this.state.value} id="input"></input>
+              <button >Send</button>
+            </div>
+
+          </form>
+        </div>
+    </div>
+    );
+  }
+}
